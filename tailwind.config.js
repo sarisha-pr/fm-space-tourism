0b109d4c640361d67aa5a862a0c861031c9c4b08
Sarisha <sarisha@gruneasia.info>
2023-04-16 00:30:32 +0800

    Setup tailwind config
    - Add fonts
    - Add colors
    - Add screen sizes
    - Add custom containers

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,55 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      'xxl': '1440px'
+    },
+
+    colors: {
+      'black': '#0B0D17',
+      'gray': '#616476',
+      'purple': '#D0D6F9',
+      'white': '#FFFFFF',
+    },
+
+    fontFamily: {
+      'bellefair': ['Bellefair', 'serif'],
+      'barlow': ['Barlow Condensed', 'sans-serif'],
+    },
+    
     extend: {},
   },
-  plugins: [],
+  corePlugins: {
+    container: false
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          width: '100%',
+          paddingLeft: '15px',
+          paddingRight: '15px',
+          '@screen sm': {
+            maxWidth: '540px',
+          },
+          '@screen md': {
+            maxWidth: '660px',
+            paddingLeft: '0',
+            paddingRight: '0',
+          },
+          '@screen lg': {
+            maxWidth: '960px',
+          },
+          '@screen xl': {
+            maxWidth: '1140px',
+          },
+        }
+      })
+    }
+  ],
 }
 
